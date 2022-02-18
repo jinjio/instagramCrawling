ac92ac2ad3dd04cc15d166f009d6ba9b037c3d94
jinjio <35422448+jinjio@users.noreply.github.com>
2022-02-18 20:57:08 +0900

    .

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
 const Instagram = require('instagram-web-api')
 const axios = require('axios')
 const cheerio = require("cheerio");
+const dotenv = require("dotenv").config();
 const express = require('express') 
 const app = express() 
 const port = 4000 
 
-const { username, password } = process.env
-const client = new Instagram({ username : username, password : password});
-await client.login();
-
-let profile = await client.getProfile();
-console.log(profile);
+const username = process.env.USERNAME
+const password = process.env.PASSWORD
 
 
-const url = "https://www.instagram.com/explore/tags/%EB%AA%A8%EC%9C%B0/"
-const getHtml = async () => { 
-    console.log(url)
-    try { 
-        return await axios.get(url); 
-    } catch (error) {
-        console.log(error)
-    }
-};
+const client = new Instagram({ username: username, password: password })
+client
+  .login()
+  .then(() => {
+    client
+      .search({ query: '모윰' })
+      .then(console.log)
+  })
 
+
+
+// const url = "https://www.instagram.com/explore/tags/%EB%AA%A8%EC%9C%B0/"
+// const getHtml = async () => { 
+//     console.log(url)
+//     try { 
+//         return await axios.get(url); 
+//     } catch (error) {
+//         console.log(error)
+//     }
+// };
+// _7UhW9.vy6Bb.MMzan.KV-D4.uL8Hv.T0kll 
 // getHtml()
 //   .then(html => {
 //     const $ = cheerio.load(html.data);
-//     // const $bodyList = $("div.headline-list ul").children("li.section02");
-//     // const data = ulList.filter(n => n.title);
-//     return null
-//   })
+//     console.log($)
+  //   return null
+  // })
 
 
 app.get('/', (req, res) => { 
@@ -39,3 +46,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 }) 
+
