b4165236bb9f1a2609bf38822d292fe91df15484
jinjio <35422448+jinjio@users.noreply.github.com>
2022-02-21 17:43:34 +0900

    총 좋아요 및 댓글 수 확인

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ instagram-web-api:https://www.npmjs.com/package/instagram-web-api2#searchparams
 instagram-graph-api:
 https://developers.facebook.com/docs/instagram-api/getting-started
 https://developers.facebook.com/docs/instagram-api/guides/hashtag-search
+
+인스타그램에서 정보를 자주 요청하지 못하도록 막아둠
+
+<!-- (node:25406) UnhandledPromiseRejectionWarning: StatusCodeError: 429 - {"message":"feedback_required","spam":true,"feedback_title":"Try Again Later","feedback_message":"We limit how often you can do certain things on Instagram to protect our community. Tell us if you think we made a mistake.","feedback_url":"repute/report_problem/scraping/","feedback_appeal_label":"Tell us","feedback_ignore_label":"OK","feedback_action":"report_problem","status":"fail"} -->
+
+s
--- index.js
+++ index.js
@@ -1,49 +1,44 @@
 const Instagram = require('instagram-web-api')
 const axios = require('axios')
-const cheerio = require("cheerio");
 const dotenv = require("dotenv").config();
-const express = require('express') 
+const express = require('express'); 
 const app = express() 
 const port = 4000 
 
 const username = process.env.USERNAME
 const password = process.env.PASSWORD
 
-
 const client = new Instagram({ username: username, password: password })
-client
-  .login()
-  .then(() => {
-    client
-      .search({ query: '모윰' })
-      .then(console.log)
-  })
+
+const contentsName = "모윰흡착볼"
+
+;(async () => {
+  await client.login()
+
+  const search = await client.getPhotosByHashtag({ hashtag: contentsName })
+  const mediaCount = search.hashtag.edge_hashtag_to_media.count
+  const mediaList = search.hashtag.edge_hashtag_to_media.edges
+
+  totalLike = 0
+  totalComment = 0
+  for(var i = 0; i<mediaList.length; i++) {
+    totalLike += mediaList[i].node.edge_media_preview_like.count
+    totalComment += mediaList[i].node.edge_media_to_comment.count
+  }
+
+  console.log("mediaCount: " + mediaCount)
+  console.log("totalLike: " + totalLike)
+  console.log("totalComment: " + totalComment)
+  // console.log(search.hashtag.edge_hashtag_to_media.edges[0].node.edge_media_to_comment)
+})()
 
 
 
-// const url = "https://www.instagram.com/explore/tags/%EB%AA%A8%EC%9C%B0/"
-// const getHtml = async () => { 
-//     console.log(url)
-//     try { 
-//         return await axios.get(url); 
-//     } catch (error) {
-//         console.log(error)
-//     }
-// };
-// _7UhW9.vy6Bb.MMzan.KV-D4.uL8Hv.T0kll 
-// getHtml()
-//   .then(html => {
-//     const $ = cheerio.load(html.data);
-//     console.log($)
-  //   return null
-  // })
+// app.get('/', (req, res) => { 
+//   res.send('Hello World!') 
+// })
 
-
-app.get('/', (req, res) => { 
-  res.send('Hello World!') 
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-}) 
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// }) 
 
